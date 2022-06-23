1d7daebb6fd3ed93ea52ab712a187ee596bcc7f2
Jesus Garcia <yisusgarcia1002@gmail.com>
2022-06-23 03:37:03 -0500

    Se creo el header de la pagina junto con los componentes del perfil, solo html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AvatarComponent } from './components/shared/avatar/avatar.component';
+import { FeatureRoutingModule } from './app.routes';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AvatarComponent,
+    PerfilComponent,
+    ClienteComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeatureRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './components/home/home.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+// import { PageNotFoundComponent } from './';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'perfil', component: PerfilComponent },
+    // { path: 'path3', component: Name3Component },
+    // { path: 'path4', component: Name4Component },
+    { path: '**',pathMatch: 'full', redirectTo: 'home' }
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class FeatureRoutingModule {}
