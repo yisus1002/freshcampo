8b13042a0fc9a2999d7712bc635e12fdf7490588
Jesus Garcia <yisusgarcia1002@gmail.com>
2022-07-15 02:59:19 -0500

    Se arreglo el carrousel de img

--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nosotros.component.css']
 })
 export class NosotrosComponent implements OnInit {
-
+  img:any=[
+    {img:'https://www.caracteristicas.co/wp-content/uploads/2016/04/campo-1-e1558303217442-800x400.jpg',
+    descipcion:'Somos una organizacion encargada de conectar al sector primario productor campesino, entregando productos frescos a consumidores finales'},
+    {img: 'https://www.namesnack.com/images/Namesnack-nombres-para-chacras-de-campo-2400x1600-2021084.jpeg?crop=40:21,smart&width=1200&dpr=2',
+    descipcion:''},
+    {img:'https://static.eldiario.es/clip/21b86a87-b71c-464d-9559-13468c86f2a9_16-9-discover-aspect-ratio_default_0.jpg',
+    descipcion:''},
+  ]
   constructor() { }
 
   ngOnInit(): void {
